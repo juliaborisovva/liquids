98b3c8a0c14c8410ddf5e4a197f980f76841f41d
juliaborisovva <juliaborisovva@mail.ru>
2025-02-03 23:54:58 +0700

    Add Liquid class

---
+++ src/libliquids.hpp
@@ -0,0 +1,40 @@
+#ifndef LIBLIQUIDS_HPP
+#define LIBLIQUIDS_HPP
+
+namespace liquids {
+
+enum class ErrorCode {
+    kSuccess = 0,
+    kInvalidColor = -1,
+    kInvalidP = -2,
+    kInvalidV = -3,
+    kTooMuchV = -4,
+    kVLEZero = -5,
+    kNotFoundLiquid = -6
+};
+
+class Liquid
+{
+   private:
+    // временно значение цвета в виде uint8_t
+    uint8_t color_;
+    float p_;
+    float v_;
+
+   public:
+    Liquid(uint8_t color = 255, float p = 100.0, float v = 0.1);
+    ~Liquid();
+
+    void SetColor(uint8_t color);
+    uint8_t GetColor();
+
+    ErrorCode SetDensity(float p);
+    float GetDensity();
+
+    ErrorCode SetVolume(float v);
+    float GetVolume();
+};
+
+}  // namespace liquids
+
+#endif  // LIBLIQUIDS_HPP
---
+++ src/liquid.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <stdexcept>
+
+#include "libliquids.hpp"
+
+/* Жидкость: цвет жидкости (ргб), пока число, потом можно изменить на ргб;
+ * плотность жидкости (кило-грамм на кубический метр кг/м³, > 0);
+ * объем жидкости (в л > 0); геттер-сеттер информация о цвете жидкости;
+ * геттер-сеттер информация о плотности; геттер-сеттер информация об объеме. */
+
+namespace liquids {
+
+Liquid::Liquid(uint8_t color, float p, float v) : color_(color)
+{
+    if (p <= 0) {
+        throw std::invalid_argument("Ошибка: значение P должно быть > 0.");
+    }
+    if (v <= 0) {
+        throw std::invalid_argument("Ошибка: значение V должно быть > 0.");
+    }
+
+    this->color_ = color;
+    this->p_ = p;
+    this->v_ = v;
+}
+
+Liquid::~Liquid()
+{
+    // очищение
+}
+
+void Liquid::SetColor(uint8_t color) { color_ = color; }
+
+uint8_t Liquid::GetColor() { return this->color_; }
+
+ErrorCode Liquid::SetDensity(float p)
+{
+    ErrorCode err = ErrorCode::kInvalidP;
+    if (p > 0) {
+        err = ErrorCode::kSuccess;
+        this->p_ = p;
+    }
+    return err;
+}
+
+float Liquid::GetDensity() { return this->p_; }
+
+ErrorCode Liquid::SetVolume(float v)
+{
+    ErrorCode err = ErrorCode::kInvalidV;
+    if (v > 0) {
+        err = ErrorCode::kSuccess;
+        this->v_ = v;
+    }
+    return err;
+}
+
+float Liquid::GetVolume() { return this->v_; }
+
+}  // namespace liquids
